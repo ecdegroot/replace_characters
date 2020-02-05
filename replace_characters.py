665f7e9ea418915ab97d7341a5458d81d33c713c
ecdegroot <ecdegroot@gmail.com>
2020-02-05 18:23:22 +0100

    Update replace_characters.py

--- replace_characters.py
+++ replace_characters.py
@@ -1,5 +1,5 @@
-# This python program replaces multiple characters in a word or sentence
-# example:
+# https://github.com/ecdegroot/replace_characters
+# This python program replaces multiple characters in a word or sentence, example:
 # text_input = "Intelligence is the ability to adapt to change"
 # text_output = "1N73LL1G3NC3 15 7H3 4B1L17Y 70 4D4P7 70 CH4NG3"
 
@@ -9,7 +9,7 @@ input_str = input("Type someting: ")
 # convert all letters to uppercase
 input_str_upper = input_str.upper()
 
-# print output with replaced letters.
+# print output with replaced letters and in uppercase.
 print ( input_str_upper
         .replace ( 'I', '1' )
         .replace ( 'T', '7' )
